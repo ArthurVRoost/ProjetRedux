3a2fa1fc9a57e324e256907322b467f12e25eaac
Arthur Van Roost <arthur.vanroost@icloud.com>
2025-06-18 16:15:18 +0200

    hover cards

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,29 +1,35 @@
 import './home.css'
 import data from '/src/data/data.json'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-  import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+
 export default function Home() {
     const pizzaMap = data.map(pizza=>({
         image: pizza.image,
         name: pizza.name,
         price: pizza.price
     }))
+    
     return(
         <>
             <div className='divHome'>
                 {data.map((pizza, index)=>(
-                    <div key={index}className='homeDivP'>
+                    <div key={index} className='homeDivP'>
                         <img className='homeImg' src={pizza.image} alt={pizza.image} />
                         <div className='homeDiv1'>
                             <h3 className='homeNom'>{pizza.name}</h3>
+                            <div className='homeDescription'>
+                                    <p>{pizza.description}</p>
+                            </div>
                             <div className='homeDiv2'>
-                                <p className='homeP1'>à partir de</p>
-                                <p className='homePrix'>{pizza.price}</p>
-                                <FontAwesomeIcon className='homeIcon' icon={faPlus} />
+                                <div className='homeContent'>
+                                    <p className='homeP1'>à partir de</p>
+                                    <p className='homePrix'>€{pizza.price}</p>
+                                    <FontAwesomeIcon className='homeIcon' icon={faPlus} />
+                                </div>
+                                
                             </div>
                         </div>
-                        
-                        
                     </div>
                 ))}
             </div>
