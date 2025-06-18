5d7325b1f37b396d9ed3c1e1512c3e24caaefedb
Arthur Van Roost <arthur.vanroost@icloud.com>
2025-06-18 16:27:49 +0200

    app jsx clean

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 import './App.css'
-import Footer from './components/footer/Footer'
-import Nav from './components/nav/Nav'
-import Home from './pages/home/Home'
+
 
 function App() {
   
 
   return (
     <>
-    <Nav/>
-    <Home/>
-    <Footer/>
     </>
   )
 }
