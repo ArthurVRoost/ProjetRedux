e3ff433cea6b2c455bee4f10e312ea965e69311a
Yassine <baadiyassine01@gmail.com>
2025-06-19 16:35:07 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import './App.css'
-import Panier from './components/panier/Panier'
-import Nav from './components/nav/Nav'
 import Home from './pages/home/Home'
 import { Route, Routes } from 'react-router-dom'
 import Layout from './components/layout'
 import Fini from './pages/fini/Fini'
-import Details from './pages/details/Details'
-import Nav from './components/nav/Nav'
+
 
 
 
